f54d1300001609268770e847b7200295947ec427
FujiiWill <fujiiwiliam@gmail.com>
2024-01-21 11:30:05 -0300

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-function App() {
-  return (
-    <h1>Bom dia</h1>
-  )
-}
+import Catalog from "./routes/Catalog";
 
-export default App
+export default function App() {
+  return <Catalog />;
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import "./syles.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="ct-container">
+        <h1>Carros Top</h1>
+      </div>
+    </header>
+  );
+}
